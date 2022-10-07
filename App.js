39e0e3300338c19c76c08d4b5341568495c738e9
Priyankarale <88052104+Priyankarale@users.noreply.github.com>
2022-10-07 11:16:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  let time = new Date().toLocaleString();
+  return (
+    <div >
+      <h1>Hello</h1>
+      <h2>{time}</h2>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
